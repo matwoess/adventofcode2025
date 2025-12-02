8ae1a839cf9131d6b032f0e3b0ab8c16719e35fb
Mathias Wöß <mat.woess@gmail.com>
2025-12-02 21:53:19 +0100

    Save solution for day02 part 2

--- src/day02.kt
+++ src/day02.kt
@@ -16,9 +16,43 @@ data class IdRange(val start: Long, val end: Long) {
 		val invalidIds = mutableListOf<Long>()
 		for (id in allIds) {
 			val idString = id.toString()
-			if (idString.startsWith("0")
-				|| idString.take(idString.length / 2) == idString.substring(idString.length / 2, idString.length)
-			) {
+			if (idString.startsWith("0") || isRepeatingSubstringOfLength(idString, idString.length / 2)) {
+				invalidIds.add(id)
+			}
+		}
+		return invalidIds
+	}
+
+	fun isRepeatingSubstringOfLength(s: String, length: Int): Boolean {
+		if (length==0 || s.length % length != 0) {
+			return false
+		}
+		val substring = s.take(length)
+		var index = length
+		while (index < s.length) {
+			if (s.substring(index, index + length) != substring) {
+				return false
+			}
+			index += length
+		}
+		return true
+	}
+
+	fun anySubstringRepeats(s: String): Boolean {
+		for (length in s.length / 2 downTo 1) {
+			if (isRepeatingSubstringOfLength(s, length)) {
+				return true
+			}
+		}
+		return false
+	}
+
+	fun getInvalidIds2(): List<Long> {
+		val allIds = getIds()
+		val invalidIds = mutableListOf<Long>()
+		for (id in allIds) {
+			val idString = id.toString()
+			if (idString.startsWith("0") || anySubstringRepeats(idString)) {
 				invalidIds.add(id)
 			}
 		}
@@ -36,16 +70,21 @@ fun preProcessData(input: String): List<IdRange> {
 fun part1(ranges: List<IdRange>): Long {
 	var invalidIdSum = 0L
 	for (range in ranges) {
-		println("Processing range: ${range.start}-${range.end}")
 		val invalidIds = range.getInvalidIds()
-		println(" - found ${invalidIds.size} invalid IDs")
 		invalidIdSum += invalidIds.sum()
 	}
 	return invalidIdSum
 }
 
 fun part2(ranges: List<IdRange>): Long {
-	return 0
+	var invalidIdSum = 0L
+	for (range in ranges) {
+		println("Processing range: ${range.start}-${range.end}")
+		val invalidIds = range.getInvalidIds2()
+		println(" - found ${invalidIds.size} invalid IDs")
+		invalidIdSum += invalidIds.sum()
+	}
+	return invalidIdSum
 }
 
 
@@ -55,5 +94,5 @@ fun main() {
 	val answer1 = part1(data)
 	println("Answer 1: $answer1") // 8576933996
 	val answer2 = part2(data)
-	println("Answer 2: $answer2") // ANSWER
+	println("Answer 2: $answer2") // 25663320831
 }
