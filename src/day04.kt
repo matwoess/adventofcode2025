21c5ccb85b364d6d9a2baa171e031c439ab39218
Mathias Wöß <mat.woess@gmail.com>
2025-12-04 21:04:36 +0100

    Save part2 of day04

--- src/day04.kt
+++ src/day04.kt
@@ -16,31 +16,59 @@ fun part1(grid: Grid2D<Char>): Int {
 		.toList()
 	var validPositions = 0
 	for (aPos in aPositions) {
-		val nw = grid.getAdjacentValue(aPos, Direction.NW)
-		val n = grid.getAdjacentValue(aPos, Direction.N)
-		val ne = grid.getAdjacentValue(aPos, Direction.NE)
-		val e = grid.getAdjacentValue(aPos, Direction.E)
-		val se = grid.getAdjacentValue(aPos, Direction.SE)
-		val s = grid.getAdjacentValue(aPos, Direction.S)
-		val sw = grid.getAdjacentValue(aPos, Direction.SW)
-		val w = grid.getAdjacentValue(aPos, Direction.W)
-		if (listOf(nw, n, ne, e, se, s, sw, w).count { '@' == it } < 4) {
+		if (isValid(grid, aPos)) {
 			validPositions++
 		}
 	}
 	return validPositions
 }
 
+private fun isValid(
+	grid: Grid2D<Char>,
+	aPos: Grid2D.Position<Char>
+): Boolean {
+	val nw = grid.getAdjacentValue(aPos, Direction.NW)
+	val n = grid.getAdjacentValue(aPos, Direction.N)
+	val ne = grid.getAdjacentValue(aPos, Direction.NE)
+	val e = grid.getAdjacentValue(aPos, Direction.E)
+	val se = grid.getAdjacentValue(aPos, Direction.SE)
+	val s = grid.getAdjacentValue(aPos, Direction.S)
+	val sw = grid.getAdjacentValue(aPos, Direction.SW)
+	val w = grid.getAdjacentValue(aPos, Direction.W)
+	return listOf(nw, n, ne, e, se, s, sw, w).count { '@' == it } < 4
+}
+
 fun part2(grid: Grid2D<Char>): Int {
-    return 0
+	var grid = grid
+	var removedRolls = 0
+	var cont = true
+	while (cont) {
+		val aPositions = grid.getPositions()
+			.filter { it.el == '@' }
+			.toList()
+		val toRemove = mutableListOf<Grid2D.Position<Char>>()
+		for (aPos in aPositions) {
+			if (isValid(grid, aPos)) {
+				toRemove.add(aPos)
+			}
+		}
+		if (toRemove.isEmpty()) {
+			cont = false
+		} else {
+			removedRolls+= toRemove.size
+			val values = toRemove.map { '.' }
+			grid = grid.copyWithModifications(toRemove, values)
+		}
+	}
+	return removedRolls
 }
 
 
 fun main() {
-    val input = File("inputs/day04.txt").readText()
-    val data = preProcessData(input)
-    val answer1 = part1(data)
-    println("Answer 1: $answer1") // ANSWER
-    val answer2 = part2(data)
-    println("Answer 2: $answer2") // ANSWER
+	val input = File("inputs/day04.txt").readText()
+	val data = preProcessData(input)
+	val answer1 = part1(data)
+	println("Answer 1: $answer1") // 1464
+	val answer2 = part2(data)
+	println("Answer 2: $answer2") // 8409
 }
--- src/util/Direction.kt
+++ src/util/Direction.kt
@@ -1,17 +1,17 @@
 package util
 
 enum class Direction {
-    N, S, W, E, NW, NE, SW, SE;
+	N, S, W, E, NW, NE, SW, SE;
 
-    fun directionalOffset(): Pair<Int, Int> = when (this) {
-        N -> Pair(0, -1)
-        S -> Pair(0, 1)
-        W -> Pair(-1, 0)
-        E -> Pair(1, 0)
-        NW -> Pair(-1, -1)
-        NE -> Pair(1, -1)
-        SW -> Pair(-1, 1)
-        SE -> Pair(1, 1)
+	fun directionalOffset(): Pair<Int, Int> = when (this) {
+		N -> Pair(0, -1)
+		S -> Pair(0, 1)
+		W -> Pair(-1, 0)
+		E -> Pair(1, 0)
+		NW -> Pair(-1, -1)
+		NE -> Pair(1, -1)
+		SW -> Pair(-1, 1)
+		SE -> Pair(1, 1)
 
-    }
+	}
 }
