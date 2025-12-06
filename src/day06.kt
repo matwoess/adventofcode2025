9e9ea08c2246220ab05dda41f2ece7d0404212af
Mathias Wöß <mat.woess@gmail.com>
2025-12-06 12:56:13 +0100

    Improve solution for day06 part2

--- src/day06.kt
+++ src/day06.kt
@@ -6,42 +6,37 @@ import java.io.File
 
 data class MathProblem(val numbers: List<Long>, val operator: Char) {
 	fun applyOperation(): Long {
-		when (operator) {
-			'+' -> return numbers.sum()
-			'*' -> return numbers.reduce { acc, l -> acc * l }
+		return when (operator) {
+			'+' -> numbers.sum()
+			'*' -> numbers.reduce { acc, l -> acc * l }
 			else -> throw IllegalArgumentException("Unknown operator: $operator")
 		}
 	}
 }
 
-
-fun preProcessData(lines: List<String>): List<MathProblem> {
-	val numberLists = mutableListOf<MutableList<Long>>()
+fun part1(input: String): Long {
+	val lines = input.lines()
 	val mathProblems = mutableListOf<MathProblem>()
 	val whitespaceRegex = "\\s+".toRegex()
-	for (line in lines) {
+	val numberLists = mutableListOf<MutableList<Long>>()
+	// Init number lists with amount of columns
+	repeat(lines[0].split(whitespaceRegex).size) {
+		numberLists.add(mutableListOf())
+	}
+	// Populate number lists
+	for (line in lines.dropLast(1)) {
 		val values = line.split(whitespaceRegex).map(String::trim).filter(String::isNotBlank)
-		if (numberLists.isEmpty()) {
-			for (i in values.indices) {
-				numberLists.add(mutableListOf())
-				numberLists[i].add(values[i].toLong())
-			}
-		} else if (values[0] == "+" || values[0] == "*") {
-			for (i in values.indices) {
-				mathProblems.add(MathProblem(numberLists[i], values[i].first()))
-			}
-			break
-		} else {
-			for (i in values.indices) {
-				numberLists[i].add(values[i].toLong())
-			}
+		for (i in values.indices) {
+			numberLists[i].add(values[i].toLong())
 		}
 	}
-	return mathProblems
-}
-
-fun part1(problems: List<MathProblem>): Long {
-	return problems.sumOf { it.applyOperation() }
+	// Finalize math problem creation
+	val operators = lines.last().split(whitespaceRegex).map(String::trim).filter(String::isNotBlank)
+	for (i in operators.indices) {
+		mathProblems.add(MathProblem(numberLists[i], operators[i].first())) // first == string to char
+	}
+	// Calculate final result
+	return mathProblems.sumOf { it.applyOperation() }
 }
 
 fun part2(input: String): Long {
@@ -49,7 +44,6 @@ fun part2(input: String): Long {
 	val width = grid.getWidth()
 	val mathProblems = mutableListOf<MathProblem>()
 	val numbers = mutableListOf<Long>()
-	println(grid)
 	for (col in width - 1 downTo 0) {
 		val topPosition = grid.getPositionAt(0, col)
 		val num = grid.getDirectionalValueSequence(topPosition, Direction.S).joinToString("").trim()
@@ -60,19 +54,17 @@ fun part2(input: String): Long {
 			numbers.add(num.dropLast(1).trim().toLong())
 			mathProblems.add(MathProblem(numbers.map { it }, operator))
 			numbers.clear()
-		}
-		else {
+		} else {
 			numbers.add(num.trim().toLong())
 		}
 	}
-	return part1(mathProblems)
+	return mathProblems.sumOf { it.applyOperation() }
 }
 
 
 fun main() {
 	val input = File("inputs/day06.txt").readText()
-	val data = preProcessData(input.split("\n"))
-	val answer1 = part1(data)
+	val answer1 = part1(input)
 	println("Answer 1: $answer1") // 4805473544166
 	val answer2 = part2(input)
 	println("Answer 2: $answer2") // 8907730960817
