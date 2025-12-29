2d4b41850a43901f4aba04bc893be2fda15b0a06
Mathias Wöß <mat.woess@gmail.com>
2025-12-30 00:48:33 +0100

    Improve solutions for day11

--- src/day11.kt
+++ src/day11.kt
@@ -34,21 +34,12 @@ fun preProcessData(lines: List<String>): List<Device> {
 fun part1(devices: List<Device>): Int {
 	val start = devices.find { it.name == "you" } ?: return -1
 	val leaf = devices.find { it.name == "out" } ?: return -1
-	return countValidPathsRecursive(start, leaf)
+	return countValidPathsRecursive(start, leaf, maxDepth = Int.MAX_VALUE)
 }
 
-fun countValidPathsRecursive(
-	device: Device,
-	leafDevice: Device,
-	maxDepth: Int = Int.MAX_VALUE,
-	currDepth: Int = 0
-): Int {
-	if (device == leafDevice) {
-		return 1
-	}
-	if (currDepth > maxDepth) {
-		return 0
-	}
+fun countValidPathsRecursive(device: Device, leafDevice: Device, maxDepth: Int, currDepth: Int = 0): Int {
+	if (device == leafDevice) return 1
+	if (currDepth > maxDepth) return 0
 	var sum = 0
 	for (nextDevice in device.outputs) {
 		sum += countValidPathsRecursive(nextDevice, leafDevice, maxDepth, currDepth + 1)
@@ -56,38 +47,28 @@ fun countValidPathsRecursive(
 	return sum
 }
 
-fun part2(devices: List<Device>): Long {
-	println(devices.joinToString("\n") { it.toString() })
-	val svr = devices.find { it.name == "svr" } ?: return -1
-	val dac = devices.find { it.name == "dac" } ?: return -1
-	val fft = devices.find { it.name == "fft" } ?: return -1
-	val out = devices.find { it.name == "out" } ?: return -1
-	val depthSvrToFft = bfs(svr, fft)
-	println("Depth svr to fft: $depthSvrToFft")
-	val depthFftToDac = bfs(fft, dac)
-	println("Depth fft to dac: $depthFftToDac")
-	val depthDacToOut = bfs(dac, out)
-	println("Depth dac to out: $depthDacToOut")
-
-	val dacToOut = countValidPathsRecursive(dac, out, maxDepth = depthDacToOut + 2)
-	val fftToDac = countValidPathsRecursive(fft, dac, maxDepth = depthFftToDac + 3)
-	val srvToFft = countValidPathsRecursive(svr, fft, maxDepth = depthSvrToFft + 2)
-	println("Valid paths svr to fft: $srvToFft")
-	println("Valid paths fft to dac: $fftToDac")
-	println("Valid paths dac to out: $dacToOut")
-	// Paths: svr -> fft -> dac -> out
-	val totalValidPaths = (srvToFft.toLong() * fftToDac * dacToOut)
-	return totalValidPaths
+fun part2(devices: List<Device>): String {
+	val svr = devices.find { it.name == "svr" } ?: return "-1"
+	val dac = devices.find { it.name == "dac" } ?: return "-1"
+	val fft = devices.find { it.name == "fft" } ?: return "-1"
+	val out = devices.find { it.name == "out" } ?: return "-1"
+	val depthSvrToFft = getDeviceDepthByBFS(svr, fft)
+	val depthFftToDac = getDeviceDepthByBFS(fft, dac)
+	val depthDacToOut = getDeviceDepthByBFS(dac, out)
+	val extraDepthTolerancy = 3 // magic number to allow some (not too much) extra depth for alternative paths
+	val srvToFft = countValidPathsRecursive(svr, fft, maxDepth = depthSvrToFft + extraDepthTolerancy)
+	val fftToDac = countValidPathsRecursive(fft, dac, maxDepth = depthFftToDac + extraDepthTolerancy)
+	val dacToOut = countValidPathsRecursive(dac, out, maxDepth = depthDacToOut + extraDepthTolerancy)
+	return "${srvToFft.toLong() * fftToDac * dacToOut} (srv-fft:$srvToFft * fft-dac:$fftToDac * dac-out:$dacToOut)"
 }
 
-fun bfs(start: Device, toNode: Device): Int {
+fun getDeviceDepthByBFS(start: Device, targetNode: Device): Int {
 	val queue = ArrayDeque(listOf(Pair(start, 0)))
 	val visited = mutableSetOf<Device>()
 	visited.add(start)
 	while (queue.isNotEmpty()) {
 		val (current, depth) = queue.removeFirst()
-		if (current == toNode) {
-			println("Found target node ${current.name} with a depth of $depth")
+		if (current == targetNode) {
 			return depth
 		}
 		visited.add(current)
@@ -97,33 +78,8 @@ fun bfs(start: Device, toNode: Device): Int {
 			}
 		}
 	}
-	return -1
+	throw IllegalStateException("No path from $start to $targetNode found")
 }
-
-//fun findPathsToOut(device: Device, foundDac: Boolean, foundFft: Boolean): Int {
-//	var foundDac = foundDac
-//	if (!foundDac && device.name == "dac") {
-//		foundDac = true
-//	}
-//	var foundFft = foundFft
-//	if (!foundFft && device.name == "fft") {
-//		foundFft = true
-//	}
-//	//println(currentPath.joinToString(",") { it.name })
-//	if (device.name == "out") {
-//		if (foundDac && foundFft) {
-//			return 1
-//		} else {
-//			return 0
-//		}
-//	}
-//	var sum = 0
-//	for (nextDevice in device.outputs) {
-//		sum += findPathsToOut(nextDevice, foundDac, foundFft)
-//	}
-//	return sum
-//}
-
 
 fun main() {
 	val input = File("inputs/day11.txt").readLines()
